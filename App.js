12b3365d7f1a1fa0ed56b7104f821ea2da3c4df4
Iuliia Glazeva <zhotter@mail.ru>
2017-05-29 16:44:30 +0200

    created app.post

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 var pg = require('pg');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')();
 
 app.set('views', 'Views');
 app.set('view engine', 'pug');
 
-var connectionString = 'postgres://postgres:1q2w3e@localhost/postgres';
+
+app.use('/', bodyParser);
+
+var connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/bulletin_board';
 
 app.get('/', (req, res)=>{
 	pg.connect(connectionString, function(err, client, done){
@@ -16,42 +20,34 @@ app.get('/', (req, res)=>{
 			// console.log(result);
 			console.log(result.rows);
 			done();
-			res.render('messages', {messages: result.rows
-			});
+			res.render('messages', {messages: result.rows});
 		});
-		
 	});
 	pg.end();
 	
 });
+
+
 app.get('/messages', (req, res) =>{
 	res.render('index');
 });
 
 app.post('/messages', (req, res)=> {
-	//wo sind die Daten???
-	var nachrichtTitle = req.body.title;
-	var nachrichtBody = req.body.body;
 	pg.connect(connectionString, function(err, client, done){
 		if(err){
-			throw err
+			throw err;
 		};
-
-	client.query('insert into messsage (title, body) values (' + nachrichtTitle + "," + nachrichtBody + ')', function(err, result){
-		console.log(result.rows);
-		done();
-		res.render('index', {yolo: result.rows});
-	});
-
 		
-	});
-
-	pg.end();
-		
-
-	
- });
-
+		client.query(`insert into messages (title, body) values ('${req.body.title}', '${req.body.body}')`, function(err, result){
+			client.query('select * from messages', function(err, result){
+				console.log(result.rows);
+				res.render('messages', {messages: result.rows});
+				done()
+			});
+		});
+	pg.end();	
+	});		
+});
 
 var server = app.listen(3000, function(){
 	console.log('userApp listening on port:'  + server.address().port);
